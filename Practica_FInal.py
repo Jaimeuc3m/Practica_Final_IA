b9b14b8fbb7e11e5e46735364ed868c2a6313ae8
KingJaime <75524837+KingJaime@users.noreply.github.com>
2023-05-07 20:18:00 +0200

    Avance

--- Practica_FInal.py
+++ Practica_FInal.py
@@ -1,6 +1,11 @@
 import csv
 
+acciones = ['encender', 'apagar']
+estados = []
+for i in range(160, 255, 5):
+    estados.append(i/10)
 meta = 22
+V = []
 iteraciones = 5000
 tolerancia = 1e-5
 
@@ -8,4 +13,20 @@ tolerancia = 1e-5
 with open('TPC.csv', 'r') as file:
     lector = csv.reader(file)
     for i in lector:
-        print(i)
+        coste_encender = i[0]
+        coste_apagar = i[1]
+        temp = i[2]
+        break
+
+    if temp == meta:
+        V[0] = 0
+    else:
+        while i < iteraciones:
+            i += 1
+            ecuacion_encender = coste_encender + 
+            ecuacion_apagar = coste_apagar +
+            if ecuacion_apagar <= ecuacion_encender:
+                V[i] = ecuacion_apagar
+            else:
+                V[i] = ecuacion_encender
+        
